c48089db7963690e98fc89ed862dd2ec17ced0b0
SyphonArch <jake.hyun@hotmail.com>
2021-03-12 13:25:41 +0900

    Allowed host change

--- soldier_link/settings.py
+++ soldier_link/settings.py
@@ -26,7 +26,7 @@ with open('secret_key.secret', 'r') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '54.180.144.73']
 
 
 # Application definition
