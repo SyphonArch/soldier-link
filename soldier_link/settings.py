af7195fd0c8c607122c34747823a4968849eb334
SyphonArch <jake.hyun@hotmail.com>
2021-03-12 16:42:32 +0900

    Changes

--- soldier_link/settings.py
+++ soldier_link/settings.py
@@ -24,7 +24,7 @@ with open('secret_key.secret', 'r') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '54.180.144.73']
 
